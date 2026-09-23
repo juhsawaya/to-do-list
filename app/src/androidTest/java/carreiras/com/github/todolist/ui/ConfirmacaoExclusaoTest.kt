8c2f291ab14022969da4c99842bf15b54832b242
Julia Sawaia <juliamsawaia@gmail.com>
2026-09-22 21:16:42 -0300

    fix(test): remover import de metodo membro do Compose Test

--- app/src/androidTest/java/carreiras/com/github/todolist/ui/ConfirmacaoExclusaoTest.kt
+++ app/src/androidTest/java/carreiras/com/github/todolist/ui/ConfirmacaoExclusaoTest.kt
@@ -1,7 +1,6 @@
 package carreiras.com.github.todolist.ui
 
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
